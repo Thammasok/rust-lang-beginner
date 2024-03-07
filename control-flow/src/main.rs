2037e621aaf1955b4adf021b922c5756926062a2
jaranchai <jaranchai.nt@hotmail.com>
2024-03-07 14:26:43 +0700

    [Added] control flow

---
+++ control-flow/src/main.rs
@@ -0,0 +1,125 @@
+fn main() {
+    if_condition();
+
+    if_in_let();
+
+    // loop_infinite();
+
+    loop_break();
+
+    loop_return();
+
+    loop_in_loop();
+
+    while_loop();
+
+    while_loop2();
+
+    for_loop();
+}
+
+fn if_condition() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+}
+
+fn if_in_let() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
+
+// fn loop_infinite() {
+//     loop {
+//         println!("again!");
+//     }
+// }
+
+fn loop_break() {
+    let mut counter = 0;
+    loop {
+        counter += 1;
+        if counter == 10 {
+            break;
+        }
+    }
+
+    println!("counter: {}", counter);
+}
+
+fn loop_return() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn loop_in_loop() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn while_loop() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn while_loop2() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+}
